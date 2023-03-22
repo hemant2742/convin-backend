9f7e62a4398799b0ff367e39b31e7a670337f5c5
Hemant Kumar Singh <shemant820@gmail.com>
2023-03-22 13:02:51 +0530

    update cors policies

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
-
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
 server.use(middlewares);
 server.use(router);
 
